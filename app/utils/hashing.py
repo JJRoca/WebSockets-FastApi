92c7a8d5bac9109f63a3dcb368b8acabd8456542
JJRoca <hanzoblade1234@outlook.com>
2024-10-01 14:24:23 -0400

    Revert "feat(pagination): add pagination utility"
    This reverts commit 3c4d05dd8c817ce652e17261a525e82c480c7ffc.

--- app/utils/hashing.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["argon2"],deprecated="auto")
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password(password_plane:str, password_hash: str) -> bool:
-    return pwd_context.verify(password_plane, password_hash)
